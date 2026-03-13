b13a9e2dbe58dec8440294b66487ee683fa739c8
Glads0n <164721067+Glads0n@users.noreply.github.com>
2026-03-13 13:01:36 +0530

    axios update

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -42,36 +42,51 @@
 
 // export default api;
 
+// import axios from "axios";
+
+
+// function getCSRFToken() {
+//   const name = "csrftoken=";
+//   const decodedCookie = decodeURIComponent(document.cookie);
+//   const cookies = decodedCookie.split(";");
+
+//   for (let cookie of cookies) {
+//     cookie = cookie.trim();
+//     if (cookie.startsWith(name)) {
+//       return cookie.substring(name.length);
+//     }
+//   }
+//   return null;
+// }
+
+// const api = axios.create({
+//   baseURL: "https://physiqyou.onrender.com/api/",
+//   // baseURL: "http://127.0.0.1:8000/api/",
+//   withCredentials: true,
+// });
+
+
+// api.interceptors.request.use((config) => {
+//   const csrfToken = getCSRFToken();
+//   if (csrfToken) {
+//     config.headers["X-CSRFToken"] = csrfToken;
+//   }
+//   return config;
+// });
+
+// export default api;
+
 import axios from "axios";
-
-
-function getCSRFToken() {
-  const name = "csrftoken=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookies = decodedCookie.split(";");
-
-  for (let cookie of cookies) {
-    cookie = cookie.trim();
-    if (cookie.startsWith(name)) {
-      return cookie.substring(name.length);
-    }
-  }
-  return null;
-}
 
 const api = axios.create({
   baseURL: "https://physiqyou.onrender.com/api/",
   // baseURL: "http://127.0.0.1:8000/api/",
+
   withCredentials: true,
-});
 
-
-api.interceptors.request.use((config) => {
-  const csrfToken = getCSRFToken();
-  if (csrfToken) {
-    config.headers["X-CSRFToken"] = csrfToken;
-  }
-  return config;
+  // Let axios automatically handle Django CSRF
+  xsrfCookieName: "csrftoken",
+  xsrfHeaderName: "X-CSRFToken",
 });
 
 export default api;
