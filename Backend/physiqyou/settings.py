a05b49c7fd60938b4b5a67fcdc8af4eafb62bb96
Glads0n <164721067+Glads0n@users.noreply.github.com>
2026-03-13 12:03:23 +0530

    csrf domain forcing

--- Backend/physiqyou/settings.py
+++ Backend/physiqyou/settings.py
@@ -85,6 +85,11 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_PATH = "/"
 SESSION_COOKIE_PATH = "/"
 
+CSRF_COOKIE_DOMAIN = "physiqyou.onrender.com"
+SESSION_COOKIE_DOMAIN = "physiqyou.onrender.com"
+
+CSRF_COOKIE_HTTPONLY = False
+
 ROOT_URLCONF = 'physiqyou.urls'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
