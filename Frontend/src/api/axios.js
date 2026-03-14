61dd88bb8e4c46240734e5cdebe9536a21091a3e
Glads0n <164721067+Glads0n@users.noreply.github.com>
2026-03-14 23:27:52 +0530

    axios.js update

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,91 +1,37 @@
-// // import axios from "axios";
-
-// // const api = axios.create({
-// //   baseURL: "http://localhost:8000/api/",
-// //   withCredentials: true, // REQUIRED for session auth
-// // });
-
-// // export default api;
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:8000/api/",
-//   withCredentials: true,
-// });
-
-// // 🔥 THIS IS THE IMPORTANT PART
-// api.defaults.xsrfCookieName = "csrftoken";
-// api.defaults.xsrfHeaderName = "X-CSRFToken";
-
-// export default api;
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:8000/api/",
-//   withCredentials: true,
-//   xsrfCookieName: "csrftoken",
-//   xsrfHeaderName: "X-CSRFToken",
-// });
-
-// export default api;
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:8000/api/",
-//   withCredentials: true,
-//   xsrfCookieName: "csrftoken",
-//   xsrfHeaderName: "X-CSRFToken",
-// });
-
-// export default api;
-
-// import axios from "axios";
-
-
-// function getCSRFToken() {
-//   const name = "csrftoken=";
-//   const decodedCookie = decodeURIComponent(document.cookie);
-//   const cookies = decodedCookie.split(";");
-
-//   for (let cookie of cookies) {
-//     cookie = cookie.trim();
-//     if (cookie.startsWith(name)) {
-//       return cookie.substring(name.length);
-//     }
-//   }
-//   return null;
-// }
-
-// const api = axios.create({
-//   baseURL: "https://physiqyou.onrender.com/api/",
-//   // baseURL: "http://127.0.0.1:8000/api/",
-//   withCredentials: true,
-// });
-
-
-// api.interceptors.request.use((config) => {
-//   const csrfToken = getCSRFToken();
-//   if (csrfToken) {
-//     config.headers["X-CSRFToken"] = csrfToken;
-//   }
-//   return config;
-// });
-
-// export default api;
-
 import axios from "axios";
+
+function getCookie(name) {
+  let cookieValue = null;
+
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+
+  return cookieValue;
+}
 
 const api = axios.create({
   baseURL: "https://physiqyou.onrender.com/api/",
-
   withCredentials: true,
+});
 
-  // Let axios automatically handle Django CSRF
-  xsrfCookieName: "csrftoken",
-  xsrfHeaderName: "X-CSRFToken",
+api.interceptors.request.use((config) => {
+  const csrfToken = getCookie("csrftoken");
+
+  if (csrfToken) {
+    config.headers["X-CSRFToken"] = csrfToken;
+  }
+
+  return config;
 });
 
 export default api;
